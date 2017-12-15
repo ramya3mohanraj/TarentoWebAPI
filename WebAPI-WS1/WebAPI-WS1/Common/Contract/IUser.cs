82a7ed979dcdce04b2c2ee203050cdef9dcb72f7
Ramya Mohan Raj <ramya.mohanraj@tarento.com>
2017-12-15 22:55:50 +0530

    Added Project to Git

---
+++ WebAPI-WS1/WebAPI-WS1/Common/Contract/IUser.cs
@@ -0,0 +1,16 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Common.Contract
+{
+    public interface IUser
+    {
+        bool Register(User user);
+        List<User> getAllUsers();
+        User Login(string userName, string password);
+    }
+}
---
+++ WebAPI-WS1/WebAPI.Data/Migrations/201712150918569_Initial.cs
@@ -0,0 +1,30 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        SurName = c.String(),
+                        Email = c.String(),
+                        UserName = c.String(),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
