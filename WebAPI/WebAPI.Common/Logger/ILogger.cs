81749a40b85f1a8918baf304c3812788c4a91487
sameer <sameer.sharma@tarento.com>
2017-12-22 11:10:00 +0530

    add logger

---
+++ WebAPI/WebAPI.Common/Logger/ILogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Common.Logger
+{
+    public interface ILogger<T>
+    {
+        void Debug(object message, Exception exception = null);
+        void Error(object message);
+        void logError(string message, Exception ex = null);
+        void logInfo(string message);
+    }
+}
---
+++ WebAPI/WebAPI.Common/Logger/Log4netLogger.cs
@@ -0,0 +1,37 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Common.Logger
+{
+    public class Log4netLogger<T> : ILogger<T>
+    {
+        private ILog _logger;
+        public Log4netLogger()
+        {
+            _logger = LogManager.GetLogger(typeof(T));
+        }
+        public void Debug(object message, Exception exception = null)
+        {
+            _logger.Debug(message, exception);
+        }
+
+        public void Error(object message)
+        {
+            _logger.Error(message);
+        }
+
+        public void logError(string message, Exception ex = null)
+        {
+            _logger.Error(message, ex);
+        }
+
+        public void logInfo(string message)
+        {
+            _logger.Info(message);
+        }
+    }
+}
